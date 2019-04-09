f1c58695815a81fc68bb73dd0702fd361aa1ed06
javametro <minda1949@sina.com>
2019-04-09 22:42:00 +0800

    new project: TestProject, new file: float_test.cs, .gitattributes

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float p3x = 80838.0f;
+            float p2y = -2499.0f;
+            double v321 = p3x * p2y;
+            Console.WriteLine(v321);
+        }
+    }
+}
---
+++ float_test.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+	static void Main(){
+		float p3x = 80838.0f;
+		float p2y = -2499.0f;
+		double v321 = p3x * p2y;
+		Console.WriteLine(v321);
+	}
+}
